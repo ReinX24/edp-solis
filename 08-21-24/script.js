185c1d3aa3be13604d06495720c72fedcfd35d78
Rein Solis <reinX244@gmail.com>
2024-08-21 10:08:08 +0800

    Added README file and changed js code to object listeners.

--- 08-21-24/script.js
+++ 08-21-24/script.js
@@ -8,30 +8,58 @@ const mouseOut = document.querySelector("#mouse-out");
 const mouseMove = document.querySelector("#mouse-move");
 
 // Adding event listeners to the elements on the page
-mouseClick.addEventListener("click", () => {
+// mouseClick.addEventListener("click", () => {
+//   alert("Fired when the user clicks an element.");
+// });
+
+mouseClick.onclick = () => {
   alert("Fired when the user clicks an element.");
-});
+};
 
-dblMouseClick.addEventListener("dblclick", () => {
+// dblMouseClick.addEventListener("dblclick", () => {
+//   alert("Fired when the user double-clicks an element.");
+// });
+
+dblMouseClick.ondblclick = () => {
   alert("Fired when the user double-clicks an element.");
-});
+};
 
-mouseDown.addEventListener("mousedown", () => {
+// mouseDown.addEventListener("mousedown", () => {
+//   alert("Fired when the user presses the mouse button down.");
+// });
+
+mouseDown.onmousedown = () => {
   alert("Fired when the user presses the mouse button down.");
-});
+};
 
-mouseUp.addEventListener("mouseup", () => {
+// mouseUp.addEventListener("mouseup", () => {
+//   alert("Fired when the user releases the mouse button.");
+// });
+
+mouseUp.onmouseup = () => {
   alert("Fired when the user releases the mouse button.");
-});
+};
 
-mouseOver.addEventListener("mouseover", () => {
+// mouseOver.addEventListener("mouseover", () => {
+//   alert("Fired when the user moves the mouse over an element.");
+// });
+
+mouseOver.onmouseover = () => {
   alert("Fired when the user moves the mouse over an element.");
-});
+};
 
-mouseOut.addEventListener("mouseout", () => {
+// mouseOut.addEventListener("mouseout", () => {
+//   alert("Fired when the user moves the mouse out of an element.");
+// });
+
+mouseOut.onmouseout = () => {
   alert("Fired when the user moves the mouse out of an element.");
-});
+};
 
-mouseMove.addEventListener("mousemove", () => {
+// mouseMove.addEventListener("mousemove", () => {
+//   alert("Fired when the user moves the mouse over an element.");
+// });
+
+mouseMove.onmousemove = () => {
   alert("Fired when the user moves the mouse over an element.");
-});
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Event Driven Programming
+
+### Rein Aldwin E. Solis [31 - ITE - 01]
+
+#### [08-21-24](/08-21-24/)
