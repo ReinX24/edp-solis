80cdbaec1e1effaba86bdfbd70a3227fa433e3e8
Rein <reinX244@gmail.com>
2024-08-27 23:06:59 +0800

    Added personal-practice folder

---
+++ personal-practice/form-events/script.js
@@ -0,0 +1,52 @@
+// TODO: resume at oninvalid
+
+// const input = document.querySelector('input');
+// const log = document.querySelector('#values');
+
+// input.addEventListener('input', updateValue);
+
+// function updateValue(e) {
+// 	log.textContent = e.target.value;
+// }
+
+// const noContext = document.getElementById('noContextMenu');
+
+// noContext.addEventListener('contextmenu', (e) => {
+// 	e.preventDefault();
+// });
+
+// const selectElement = document.querySelector('.ice-cream');
+// const result = document.querySelector('.result');
+
+// selectElement.addEventListener('change', (event) => {
+// 	result.textContent = `You like ${event.target.value}`;
+// });
+
+// const form = document.getElementById('form');
+
+// Event delegation
+// form.addEventListener(
+// 	'focus',
+// 	(event) => {
+// 		event.target.style.background = 'pink';
+// 	},
+// 	true
+// );
+
+// form.addEventListener(
+// 	'blur',
+// 	(event) => {
+// 		event.target.style.background = '';
+// 	},
+// 	true
+// );
+
+// const password = document.querySelector('input[type="password"');
+
+// password.addEventListener('focus', (event) => {
+// 	event.target.style.background = 'pink';
+// });
+
+// password.addEventListener('blur', (event) => {
+// 	event.target.style.background = '';
+// });
---
+++ personal-practice/keyboard-events/script.js
@@ -0,0 +1,65 @@
+const textInput = document.querySelector('#text-input');
+const log = document.querySelector('#log');
+
+// textInput.onkeydown = function (event) {
+// 	log.textContent = event.getModifierState('CapsLock');
+// };
+
+// textInput.onkeydown = function (event) {
+// 	if (event.shiftKey) {
+// 		log.textContent = 'The SHIFT key was pressed!';
+// 	} else {
+// 		log.textContent = 'The SHIFT key was NOT pressed!';
+// 	}
+// };
+
+// textInput.onkeydown = function (event) {
+// 	if (event.metaKey) {
+// 		log.textContent = 'The META key was pressed!';
+// 	} else {
+// 		log.textContent = 'The META key was NOT pressed!';
+// 	}
+// };
+
+// textInput.onkeydown = function (event) {
+// 	log.textContent = event.location;
+// };
+
+// textInput.onkeydown = function (event) {
+// 	log.textContent = event.key;
+// };
+
+// textInput.onkeydown = function (event) {
+// 	if (event.ctrlKey) {
+// 		log.textContent = 'The Ctrl key was pressed!';
+// 	} else {
+// 		log.textContent = 'The Ctrl key was NOT pressed!';
+// 	}
+// };
+
+// textInput.onkeydown = function (event) {
+// 	log.textContent = event.code;
+// };
+
+// textInput.onkeydown = function (event) {
+// 	if (event.altKey) {
+// 		log.textContent = 'The ALT key was pressed!';
+// 	} else {
+// 		log.textContent = 'The ALT key was NOT pressed!';
+// 	}
+// };
+
+// textInput.onkeyup = function (event) {
+// 	log.textContent += ` ${event.code}`;
+// 	console.log(event);
+// };
+
+// textInput.onkeypress = function (event) {
+// log.textContent += ` ${event.code}`;
+// 	console.log(event);
+// };
+
+// textInput.onkeydown = function (event) {
+// 	log.textContent += ` ${event.code}`;
+// console.log(event);
+// };
