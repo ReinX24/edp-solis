ee31374b9419102107b99fa52a63f8cb90d17b96
Rein <reinX244@gmail.com>
2024-11-23 00:23:50 +0800

    Added other ajax and load methods to practice script.

--- personal-practice/ajax-practice-semi-final/practice-script.js
+++ personal-practice/ajax-practice-semi-final/practice-script.js
@@ -1,5 +1,6 @@
 //* AJAX GET method
 // https://jsonplaceholder.typicode.com/posts
+// xhr: XMLHttpRequest
 $("#ajax-get").click(function () {
 	$.ajax({
 		url: "https://jsonplaceholder.typicode.com/posts",
@@ -7,11 +8,51 @@ $("#ajax-get").click(function () {
 		success: function (response) {
 			console.log(response);
 		},
-		error: function (xhr, textStatus, errorText) {
-            console.log(xhr, textStatus, errorText);
+		error: function (xhr, statusText, errorText) {
+			console.log(xhr, statusText, errorText);
 		},
 	});
 });
 
 //* AJAX POST method
 // https://jsonplaceholder.typicode.com/posts
+$("#ajax-post").click(function () {
+	$.ajax({
+		url: "https://jsonplaceholder.typicode.com/posts",
+		method: "POST",
+		data: {
+			name: "Rein",
+			age: 21,
+		},
+		success: function (response) {
+			console.log(response);
+		},
+		error: function (xhr, responseText, errorText) {
+			console.log(xhr, responseText, errorText);
+		},
+	});
+});
+
+//* LOAD GET method
+// https://jsonplaceholder.typicode.com/posts
+$("#load-get").click(function () {
+	$("#content").load(
+		"https://jsonplaceholder.typicode.com/posts",
+		function (response) {
+			// Returns a JSON object, so we have to parse the data
+			console.log(JSON.parse(response));
+		}
+	);
+});
+
+//* LOAD POST method
+// https://jsonplaceholder.typicode.com/posts
+$("#load-post").click(function () {
+	$("#content").load(
+		"https://jsonplaceholder.typicode.com/posts",
+		{ name: "Rein", age: 21 },
+		function (response) {
+			console.log(JSON.parse(response));
+		}
+	);
+});
