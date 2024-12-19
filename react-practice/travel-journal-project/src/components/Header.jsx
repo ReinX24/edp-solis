f8949422a2474d7e02452c4eecbd7b40609542f3
Rein <reinX244@gmail.com>
2024-12-19 23:58:03 +0800

    Finished header, currently on entry section.

---
+++ react-practice/travel-journal-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+	return (
+		<header id="header">
+			<img src="/src/assets/globe.png" alt="globe icon" />
+			<h1>my travel journal.</h1>
+		</header>
+	);
+}
+
+export default Header;
--- react-practice/travel-journal-project/src/main.jsx
+++ react-practice/travel-journal-project/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-
-root.render(<h1>Travel Journal Project</h1>);
+root.render(<App />);
