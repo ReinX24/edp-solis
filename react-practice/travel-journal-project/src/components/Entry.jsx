1be14e3fd1a603488f824340935b17ebe6616349
Rein <reinX244@gmail.com>
2025-01-07 14:03:51 +0800

    Changed html of travel-journal-project.

--- react-practice/travel-journal-project/src/components/Entry.jsx
+++ react-practice/travel-journal-project/src/components/Entry.jsx
@@ -1,18 +1,23 @@
 function Entry() {
 	return (
-		<div id="entry-container">
-			<img src="" alt="" />
+		<artitcle id="entry-container">
+			<img
+				src="https://scrimba.com/links/travel-journal-japan-image-url"
+				alt="mount fuji"
+			/>
 			<div>
 				<div id="google-maps-container">
 					<img src="/src/assets/marker.png" alt="marker icon" />
-					<p>JAPAN</p>
+					<span>JAPAN</span>
 					<a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
 						View on Google Maps
 					</a>
 				</div>
+
 				<div>
 					<h1>Mount Fuji</h1>
 				</div>
+
 				<div>
 					<p>12 Jan, 2021 - 24 Jan, 2021</p>
 					<p>
@@ -23,7 +28,7 @@ function Entry() {
 					</p>
 				</div>
 			</div>
-		</div> // End of entry container
+		</artitcle> // End of entry container
 	);
 }
 
